cd5e98887af31a2cd56a4e706432a54aa2fc701b
Matthew Tremper <Matthew.Tremper@ipreo.com>
2020-02-12 20:26:04 -0500

    Clean up example

--- examples/main.go
+++ examples/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/lampjaw/censusgo"
 )
 
 func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
 	b := censusgo.NewQueryBuilder("LampjawScraper", "ps2")
 	q := b.NewQuery("character")
 	q.Where("name.first_lower").Equals("lampjaw")
